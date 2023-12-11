7b27c05dc36672268c577455dbfdef004f66434a
Rodrigo Larenas <r.larenas2@alumnos.santotomas.cl>
2023-12-10 22:32:47 -0300

    Initial commit

--- app/src/main/java/com/example/urbanmarket/activities/SplashActivity.java
+++ app/src/main/java/com/example/urbanmarket/activities/SplashActivity.java
@@ -18,7 +18,7 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashActivity.this, IncorporacionActivity.class);
+                Intent intent = new Intent(SplashActivity.this, RegistrarActivity.class);
                 startActivity(intent);
                 finish();
             }
